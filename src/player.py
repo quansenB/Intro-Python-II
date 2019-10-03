c6bec9add2d20aaf2d22b365c68359ef2ddbc583
quansenB <inaki.breinbauer@gmail.com>
2019-10-03 17:17:21 +0200

    additmes and string representation

--- src/player.py
+++ src/player.py
@@ -6,3 +6,18 @@ class Player:
     def __init__(self, name, currrent_room):
         self.name = name
         self.current_room = currrent_room
+        self.items = []
+
+    def addItem(self, item):
+        self.items.append(item)
+
+    def dropItem(self, item):
+        return self.items.remove(item)
+
+    def printItems(self):
+        string = "You have the following items in your inventory: "
+        for item in self.items:
+            string += item.name
+            string += ", "
+        string = string[:-2]
+        print(string)
--- src/room.py
+++ src/room.py
@@ -10,3 +10,18 @@ class Room:
         self.e_to = e_to
         self.s_to = s_to
         self.w_to = w_to
+        self.items = []
+
+    def addItem(self, item):
+        self.items.append(item)
+
+    def takeItem(self, item):
+        return self.items.remove(item)
+
+    def printItems(self):
+        string = "In this room you can take the following items: "
+        for item in self.items:
+            string += item.name
+            string += ", "
+        string = string[:-2]
+        print(string)
